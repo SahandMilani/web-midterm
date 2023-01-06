4759186baa6302af30849bf621e5fc1e1362ce12
sahandak.milani <59655828+SahandMilani@users.noreply.github.com>
2023-01-06 19:41:42 +0330

    project core finished

--- js/index.js
+++ js/index.js
@@ -1,134 +1,204 @@
-const nameInput = document.querySelector('#name');
-const submitButton = document.querySelector('.submit');
-const saveButton = document.querySelector('.save');
-const clearButton = document.querySelector('.clear');
-const predictionGender = document.querySelector('.prediction_gender');
-const predictionPercent = document.querySelector('.prediction_percent');
-const savedAnswerCard = document.querySelector('.saved_card');
-const savedAnswerContent = document.querySelector('.saved_answer_content');
-const maleRB = document.getElementById('male');
-const femaleRB = document.getElementById('female');
-const actionResult = document.querySelector('.action_result');
+// const bio = document.getElementById("bio");
+// bio.innerHTML =
+// 	"Backend Developer,\r\nPh.D. Student,\r\nHappy and Smiling".replaceAll(
+// 		"\r\n",
+// 		"<br>"
+// 	);
+const bio = document.getElementById("bio");
+const fullName = document.getElementById("name");
+const blog = document.getElementById("blog");
+const userLocation = document.getElementById("location");
+const faveLang = document.getElementById("fave-lang");
+const avatar = document.getElementById("avatar");
+const username = document.getElementById("username");
+const btn = document.getElementById("btn");
 
+btn.onclick = (e) => {
+	e.preventDefault();
+	getUserInfo(username.value);
+};
 
+async function getUserInfo(username) {
+	console.log(localStorage.getItem("userInfo"));
+	if (localStorage.getItem("userInfo") != null) {
+		readFromLs();
+		console.log("read from cache.");
+		return;
+	}
+	console.log("fetching from github...");
+	url = `https://run.mocky.io/v3/5a1c27f0-9faf-414e-8de1-a8b21b3cc25d/${username}`;
+	const response = await fetch(url);
+	if (!response.ok) {
+		const message = `An error has occured: ${response.status}`;
+		throw new Error(message);
+	}
+	const data = await response.json();
+	console.log(data);
+	saveInLS(data);
+	readFromLs();
+}
+
+function saveInLS(data) {
+	window.localStorage.setItem("userInfo", JSON.stringify(data));
+}
+function readFromLs() {
+	data = JSON.parse(window.localStorage.getItem("userInfo"));
+
+	fullName.innerHTML = data.name;
+	blog.innerHTML = data.blog;
+	userLocation.innerHTML = data.location;
+	fullName.innerHTML = data.name;
+	avatar.src = data.avatar_url;
+	bio.innerHTML = data.bio.replaceAll("\r\n", "<br>");
+}
+
+const nameInput = document.querySelector("#name");
+const submitButton = document.querySelector(".submit");
+const saveButton = document.querySelector(".save");
+const clearButton = document.querySelector(".clear");
+const predictionGender = document.querySelector(".prediction_gender");
+const predictionPercent = document.querySelector(".prediction_percent");
+const savedAnswerCard = document.querySelector(".saved_card");
+const savedAnswerContent = document.querySelector(".saved_answer_content");
+const maleRB = document.getElementById("male");
+const femaleRB = document.getElementById("female");
+const actionResult = document.querySelector(".action_result");
 
 // send request to server and get data for input name then call functions to show result
 async function getGender(e) {
-    let name = nameInput.value;
-    e.preventDefault();
-    if (checkValidity(name)) {
-        try {
-            let response = await fetch(`https://api.genderize.io/?name=${name}`);
-            let obj = await response.json();
-            if (response.status != 200) {
-                return Promise.reject(`Request failed with error ${response.status}`);
-            }
-            setPrediction(obj);
-            let data = await JSON.parse(window.localStorage.getItem(name));
-            console.log(data);
-            if (data != null) {
-                savedAnswerCard.style.display = "block";
-                setSavedAnswer(data);
-            } else {
-                savedAnswerCard.style.display = "none";
-            }
-        } catch (e) {
-            console.log(e);
-        }
-    } else {
-        showAlert("Invalid input!");
-    }
+	let name = nameInput.value;
+	e.preventDefault();
+	if (checkValidity(name)) {
+		try {
+			let response = await fetch(
+				`https://api.genderize.io/?name=${name}`
+			);
+			let obj = await response.json();
+			if (response.status != 200) {
+				return Promise.reject(
+					`Request failed with error ${response.status}`
+				);
+			}
+			setPrediction(obj);
+			let data = await JSON.parse(window.localStorage.getItem(name));
+			console.log(data);
+			if (data != null) {
+				savedAnswerCard.style.display = "block";
+				setSavedAnswer(data);
+			} else {
+				savedAnswerCard.style.display = "none";
+			}
+		} catch (e) {
+			console.log(e);
+		}
+	} else {
+		showAlert("Invalid input!");
+	}
 }
 
 // show Prediction result to user
 function setPrediction(obj) {
-    if (obj.gender == null) {
-        predictionGender.innerHTML = '<span><i class="fas fa-ban"></i></span>';
-        predictionPercent.innerHTML = '<span><i class="fas fa-question"></i></span>';
-        showAlert("Can't Find!");
-    } else {
-        if (obj.gender == "male") {
-            var icon = '<span><i class="fas fa-male"></i><span>';
-        } else if (obj.gender == "female") {
-            var icon = '<span><i class="fas fa-female"></i><span>';
-        }
-        predictionGender.innerHTML = "<span>" + icon + "  " + obj.gender + "</span>";
-        predictionPercent.innerHTML = '<span><i class="fas fa-percent" style="font-size:10px" ></i>  ' + (obj.probability * 100) + '</span>';
-    }
+	if (obj.gender == null) {
+		predictionGender.innerHTML = '<span><i class="fas fa-ban"></i></span>';
+		predictionPercent.innerHTML =
+			'<span><i class="fas fa-question"></i></span>';
+		showAlert("Can't Find!");
+	} else {
+		if (obj.gender == "male") {
+			var icon = '<span><i class="fas fa-male"></i><span>';
+		} else if (obj.gender == "female") {
+			var icon = '<span><i class="fas fa-female"></i><span>';
+		}
+		predictionGender.innerHTML =
+			"<span>" + icon + "  " + obj.gender + "</span>";
+		predictionPercent.innerHTML =
+			'<span><i class="fas fa-percent" style="font-size:10px" ></i>  ' +
+			obj.probability * 100 +
+			"</span>";
+	}
 }
 
 // show SavedAnswer to user
 function setSavedAnswer(obj) {
-    if (obj.gender == "male") {
-        var icon = '<span><i class="fas fa-male"></i><span>';
-    } else if (obj.gender == "female") {
-        var icon = '<span><i class="fas fa-female"></i><span>';
-    }
-    savedAnswerContent.innerHTML = "<span>" + icon + "  " + obj.gender + "</span><br>" + '<span><i class="fas fa-percent" style="font-size:10px" ></i>  ' + (obj.probability * 100) + '</span>';
-
+	if (obj.gender == "male") {
+		var icon = '<span><i class="fas fa-male"></i><span>';
+	} else if (obj.gender == "female") {
+		var icon = '<span><i class="fas fa-female"></i><span>';
+	}
+	savedAnswerContent.innerHTML =
+		"<span>" +
+		icon +
+		"  " +
+		obj.gender +
+		"</span><br>" +
+		'<span><i class="fas fa-percent" style="font-size:10px" ></i>  ' +
+		obj.probability * 100 +
+		"</span>";
 }
 
 // save prediction result or user idea in local storage
 async function savePrediction(e) {
-    let name = nameInput.value;
-    let maleChecked = maleRB.checked;
-    console.log(maleChecked);
-    let femaleChecked = femaleRB.checked;
-    console.log(femaleChecked);
-    e.preventDefault();
-    if (checkValidity(name)) {
-        if (maleChecked || femaleChecked) {
-            const userObj = {
-                name: name,
-                gender: maleChecked ? "male" : "female",
-                probability: 1,
-                count: 1
-            };
-            window.localStorage.setItem(name, JSON.stringify(userObj));
-        } else {
-            let response = await fetch(`https://api.genderize.io/?name=${name}`);
-            let obj = await response.json();
-            window.localStorage.setItem(name, JSON.stringify(obj));
-        }
+	let name = nameInput.value;
+	let maleChecked = maleRB.checked;
+	console.log(maleChecked);
+	let femaleChecked = femaleRB.checked;
+	console.log(femaleChecked);
+	e.preventDefault();
+	if (checkValidity(name)) {
+		if (maleChecked || femaleChecked) {
+			const userObj = {
+				name: name,
+				gender: maleChecked ? "male" : "female",
+				probability: 1,
+				count: 1,
+			};
+			window.localStorage.setItem(name, JSON.stringify(userObj));
+		} else {
+			let response = await fetch(
+				`https://api.genderize.io/?name=${name}`
+			);
+			let obj = await response.json();
+			window.localStorage.setItem(name, JSON.stringify(obj));
+		}
 
-        showAlert("Saved!");
-    } else {
-        showAlert("Can not Save!");
-    }
+		showAlert("Saved!");
+	} else {
+		showAlert("Can not Save!");
+	}
 }
 
 // remove saved answer
 function removeSavedAnswer(e) {
-    let name = nameInput.value;
-    e.preventDefault();
-    window.localStorage.removeItem(name);
-    showAlert("Removed!");
-    savedAnswerCard.style.display = "none";
+	let name = nameInput.value;
+	e.preventDefault();
+	window.localStorage.removeItem(name);
+	showAlert("Removed!");
+	savedAnswerCard.style.display = "none";
 }
 
 // this function check input name validity
 function checkValidity(name) {
-    const regex1 = /[A-Za-z ]+/g;
-    const regex2 = /[0-9\.\-\/]+/g;
-    const foundValid = name.match(regex1);
-    const foundNotValid = name.match(regex2);
-    if (foundNotValid == null && foundValid.length > 0) {
-        return true;
-    }
-    return false;
-
+	const regex1 = /[A-Za-z ]+/g;
+	const regex2 = /[0-9\.\-\/]+/g;
+	const foundValid = name.match(regex1);
+	const foundNotValid = name.match(regex2);
+	if (foundNotValid == null && foundValid.length > 0) {
+		return true;
+	}
+	return false;
 }
 
-// show error 
+// show error
 function showAlert(title) {
-    actionResult.style.display = "block";
-    actionResult.innerHTML = "<span>" + title + "</span>";
-    setTimeout(() => { // removes the error message from screen after 4 seconds.
-        actionResult.style.display = "none";
-    }, 4000);
+	actionResult.style.display = "block";
+	actionResult.innerHTML = "<span>" + title + "</span>";
+	setTimeout(() => {
+		// removes the error message from screen after 4 seconds.
+		actionResult.style.display = "none";
+	}, 4000);
 }
 
-submitButton.addEventListener('click', getGender);
-saveButton.addEventListener('click', savePrediction);
-clearButton.addEventListener('click', removeSavedAnswer);
+submitButton.addEventListener("click", getGender);
+saveButton.addEventListener("click", savePrediction);
+clearButton.addEventListener("click", removeSavedAnswer);
 window.localStorage.clear();
